ee614906e97d449a59f6482d3f94028d61323bc2
ying <18670339700@163.com>
2017-07-03 07:50:47 +0800

    quandi

--- app/src/main/java/com/thirdworld/rest/QuanDi.java
+++ app/src/main/java/com/thirdworld/rest/QuanDi.java
@@ -1,6 +1,8 @@
 package com.thirdworld.rest;
 
 
+import com.thirdworld.entity.events.LogStr;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
@@ -91,7 +93,7 @@ public class QuanDi implements Runnable {
     public static void main(String... args) {
 //        log(getDate("2017-06-26 19:28:09").toString());
         QuanDi quanDi = new QuanDi();
-        quanDi.account = "hy814";
+        quanDi.account = "hy812";
         quanDi.pwd = "123";
         new Thread(quanDi).start();
     }
@@ -247,95 +249,38 @@ public class QuanDi implements Runnable {
 
     public void processResponse(String res) {
         String[] split = res.split("〓");
+        Map map;
         if (split.length > 0) {
             switch (split[0]) {
                 case "帮派读取圈地副本返回":
-//帮派读取圈地副本返回〓hy814〓0〓4〓1000〓〓2017年5月22日9时25分44秒〓120〓7
-//帮派读取圈地副本返回〓hy814〓0〓47〓0〓〓2017年6月26日19时22分9秒〓240〓7
-//帮派读取圈地副本返回〓hy814〓20005〓47〓4930〓2017-06-26 19:28:09〓2017年6月26日19时33分27秒〓243〓
 //帮派读取圈地副本返回〓hy814〓10091〓4〓7320〓2017-06-26 19:02:12〓2017年6月26日20时51分43秒〓26〓7
-//帮派读取圈地副本返回〓hy812〓10025〓78〓2721〓2017-06-27 11:45:52〓2017年6月27日10时20分15秒〓137〓7
-                    //账号
-                    mAccount = split[1];
-                    //最新坐标
-                    最新坐标(split[2]);
-                    //用户id
-                    mId = str2Int(split[3]);
-                    //圈地资金
-                    mZijin = str2Int(split[4]);
-                    //下次可操作时间
-                    mTimeCanOper = getDate(split[5]);
-                    //当前时间
-                    mTimeCur = getDate2(split[6]);
-                    //机动力
-                    mTimes = str2Int(split[7]);
-                    //本周个人最高可占领地皮数
-                    mMaxBuild = str2Int(split[8]);
-                    log("最新坐标:" + mCurMapZuobiao
-                            + "-用户id：" + mId
-                            + "-下次可操作时间：" + mTimeCanOper.toString()
-                            + "-当前时间：" + mTimeCur.toString() + getWeekDays()
-                            + "-机动力：" + mTimes
-                            + "-资金：" + mZijin
-                            + "-本周个人最高可占领地皮数：" + mMaxBuild
-                    );
-                    if (mZijin <= 0) {
-                        setCmd(cmdEnable);
-                    } else {
-                        sendCmd(cmdReadS);
-                    }
+                    onReadPersonInfoRsp(split);
+
+                    break;
+                case "帮派圈地开通本周成功":
+                //帮派圈地开通本周成功〓hy812〓3244.117
+                    zhiShaiZi();
+//                    zhiDing(6);
+
                     break;
                 case "盟军圈地读最新返回":
 //盟军圈地读最新返回〓战神台 80 76 3 23 3 25 2；铜人殿 20 0 0 0 0 0 0；镜雪原 30 0 0 0 0 0 0；弄梅洞窟 80 12 3 15 3 18 1；北海龙宫 70 0 0 0 0 0 0；海底皇宫 70 0 0 0 0 0 0；落雁谷 20 0 0 0 0 0 0；吊烤地狱 50 0 0 0 0 0 0；最后的房间 70 0 0 0 0 0 0；衡天台 90 91 1 158 1 5 2；巨菇洞 20 0 0 0 0 0 0；西天界 90 37 1 38 1 10 2；绿洲东郊 20 0 0 0 0 0 0；仙界天网 90 3 3 21 1 198 2；宁静海 70 0 0 0 0 0 0；万劫谷 80 157 2 69 4 158 2；剥皮地狱 50 0 0 0 0 0 0；名剑山庄正院 60 0 0 0 0 0 0；炮烙地狱 50 0 0 0 0 0 0；紫阳门 90 91 3 14 2 38 2；太和殿 30 0 0 0 0 0 0；守望林场 40 0 0 0 0 0 0；小小房间 70 0 0 0 0 0 0；冰清桥 30 0 0 0 0 0 0；随机事件 9999999 0 0 0 0 0 0；铡头地狱 50 0 0 0 0 0 0；赏月谷 20 0 0 0 0 0 0；飞升门 90 23 1 3 3 49 2；沉信当铺 70 0 0 0 0 0 0；闲情园 10 0 0 0 0 0 0；逐影洞窟 80 16 1 115 2 40 2；暴走地狱 50 0 0 0 0 0 0；雪融谷 30 0 0 0 0 0 0；鸟鸣谷 30 0 0 0 0 0 0；铁索桥 50 0 0 0 0 0 0；迷幻洞窟 80 12 2 21 1 9 2；污秽地狱 50 0 0 0 0 0 0；神将壁 80 16 2 40 1 157 3；松竹林 10 0 0 0 0 0 0；皇城甬道 80 74 2 5 2 18 1；空灵园 90 10 4 22 2 49 2；流沙原 20 0 0 0 0 0 0；枯井原 10 0 0 0 0 0 0；邂逅洞窟 80 198 4 9 2 76 3；灵丹室 90 105 3 14 1 15 3；碧水丹心谷 40 0 0 0 0 0 0；磨骨地狱 50 0 0 0 0 0 0；羁马谷 20 0 0 0 0 0 0；双印谷 40 0 0 0 0 0 0；深深海 70 0 0 0 0 0 0；旋割地狱 50 0 0 0 0 0 0；千佛殿 60 0 0 0 0 0 0；冰封地狱 50 0 0 0 0 0 0；独木原 30 0 0 0 0 0 0；观日桥 10 0 0 0 0 0 0；梦佛窟 20 0 0 0 0 0 0；吓死人洞窟1 10 0 0 0 0 0 0；逆之往事 40 0 0 0 0 0 0；银罐谷 20 0 0 0 0 0 0；名剑山庄前厅 60 0 0 0 0 0 0；浮影水城外郊 10 0 0 0 0 0 0；南海龙宫 70 0 0 0 0 0 0；宁静客栈 70 0 0 0 0 0 0；天灯地狱 50 0 0 0 0 0 0；仙临台 90 22 1 114 2 6 1；酒鬼冢 20 0 0 0 0 0 0；执手洞窟 80 4 1 105 1 25 2；石笋洞窟 80 69 2 50 1 0 0；怨灵洞口 20 0 0 0 0 0 0；炉烧地狱 50 0 0 0 0 0 0；古城遗址 20 0 0 0 0 0 0；西码头 10 0 0 0 0 0 0；猎人洞窟 10 0 0 0 0 0 0；随机事件 9999999 0 0 0 0 0 0；紫禁城 30 0 0 0 0 0 0；油锅地狱 50 0 0 0 0 0 0；圣泉台 90 115 3 163 2 0 0；黄泥岗 30 0 0 0 0 0 0；御风台 90 50 1 0 0 0 0；大殿 30 0 0 0 0 0 0；三清殿 60 0 0 0 0 0 0；怡心谷 80 4 2 74 1 114 3；守候洞窟 80 163 2 0 0 0 0；华山顶 60 0 0 0 0 0 0；很窄的过道 70 0 0 0 0 0 0；明月桥 10 0 0 0 0 0 0；东码头 10 0 0 0 0 0 0；盘龙洞窟 80 0 0 0 0 0 0；晚枫林 40 0 0 0 0 0 0；遗恨原 20 0 0 0 0 0 0；凰之谷 40 0 0 0 0 0 0；翔云幻城 90 6 1 54 3 0 0；钉板地狱 50 0 0 0 0 0 0；通仙之境 90 37 2 19 1 54 1；寒月台 10 0 0 0 0 0 0；武当练功房 60 0 0 0 0 0 0；净沙原 20 0 0 0 0 0 0；四桥原 10 0 0 0 0 0 0；紫宵宫 60 0 0 0 0 0 0；地焰洞 40 0 0 0 0 0 0；西海龙宫 70 0 0 0 0 0 0；青之绿洲 20 0 0 0 0 0 0；碧水洞窟 80 0 0 0 0 0 0；蓬莱仙境 90 51 1 0 0 0 0；深蓝海底 70 0 0 0 0 0 0；华山亭 60 0 0 0 0 0 0；斧青谷 30 0 0 0 0 0 0；仙客厅 90 0 0 0 0 0 0；春水桥 30 0 0 0 0 0 0；华山瀑布 60 0 0 0 0 0 0；金钟洞窟 80 0 0 0 0 0 0；古铜洞窟 80 0 0 0 0 0 0；雪狼湖 70 0 0 0 0 0 0；树顶 10 0 0 0 0 0 0；石壁洞口 10 0 0 0 0 0 0；逆之轮回 40 0 0 0 0 0 0；青灵洞 20 0 0 0 0 0 0；紫苔洞窟 80 51 1 0 0 0 0；名剑山庄中院 60 0 0 0 0 0 0；观日桥头 10 0 0 0 0 0 0；徘徊林 40 0 0 0 0 0 0；少林寺前院 60 0 0 0 0 0 0；随机事件 9999999 0 0 0 0 0 0；深蓝海城 70 0 0 0 0 0 0；青风原 10 0 0 0 0 0 0；东海龙宫 70 0 0 0 0 0 0；雪狼崖 70 0 0 0 0 0 0；百草池 40 0 0 0 0 0 0；榕盘谷 40 0 0 0 0 0 0；华山脚 60 0 0 0 0 0 0；敕勒川 30 0 0 0 0 0 0；不悔洞窟 80 0 0 0 0 0 0；沙盗洞口 20 0 0 0 0 0 0；坠泪岗 10 0 0 0 0 0 0；飘渺仙境 90 0 0 0 0 0 0；一点点杂货 70 0 0 0 0 0 0；松雪野 30 0 0 0 0 0 0；名剑山庄书房 60 0 0 0 0 0 0；炊烟谷 30 0 0 0 0 0 0；情人谷 20 0 0 0 0 0 0；阳光渔栈 40 0 0 0 0 0 0；窄窄的过道 70 0 0 0 0 0 0；藏经阁 60 0 0 0 0 0 0；伤心洞窟 80 19 2 0 0 0 0；剑池谷 10 0 0 0 0 0 0；天王殿 60 0 0 0 0 0 0；金轮谷 20 0 0 0 0 0 0
                     processMapData(split[1]);
-//                    build();
-                    setCmd(cmdZhiSZ);
-//                    zhiDing(6);
+                    if (mCurMapIndex>0) {
+                        map = mMaps.get(mCurMapIndex - 1);
+                        log(map.toString());
+                        if (!map.BuildIfGood()) {
+                            zhiShaiZi();
+                        }
+                    } else {
+                        zhiShaiZi();
+                    }
 
                     break;
                 case "盟军圈地掷骰子成功返回":
 //盟军圈地掷骰子成功返回〓10009〓9〓2017-05-22 09:25:45〓119〓0〓0〓1000〓9〓0〓无〓赏月谷 20 0 0 0 0 0 0；西海龙宫 70 0 0 0 0 0 0；衡天台 90 0 0 0 0 0 0；华山腰道 60 0 0 0 0 0 0；万劫谷 80 0 0 0 0 0 0；猎人洞窟 10 0 0 0 0 0 0；怨灵洞口 20 0 0 0 0 0 0；炮烙地狱 50 0 0 0 0 0 0；弄梅洞窟 80 0 0 0 0 0 0；华山亭 60 0 0 0 0 0 0；油锅地狱 50 0 0 0 0 0 0；春水桥 30 0 0 0 0 0 0；海底皇宫 70 0 0 0 0 0 0；石壁洞口 10 0 0 0 0 0 0；飞升门 90 0 0 0 0 0 0；小小房间 70 0 0 0 0 0 0；金轮谷 20 0 0 0 0 0 0；逐影洞窟 80 0 0 0 0 0 0；圣宴室 90 0 0 0 0 0 0；碧水洞窟 80 0 0 0 0 0 0；战神台 80 0 0 0 0 0 0；钉床地狱 50 0 0 0 0 0 0；寒月台 10 0 0 0 0 0 0；紫宵宫 60 0 0 0 0 0 0；随机事件 9999999 0 0 0 0 0 0；双印谷 40 0 0 0 0 0 0；皇城甬道 80 0 0 0 0 0 0；执手洞窟 80 0 0 0 0 0 0；深深海 70 0 0 0 0 0 0；枯井原 10 0 0 0 0 0 0；深蓝海城 70 0 0 0 0 0 0；灵丹室 90 0 0 0 0 0 0；过道 70 0 0 0 0 0 0；绿幽洞 20 0 0 0 0 0 0；名剑山庄书房 60 0 0 0 0 0 0；沉信当铺 70 0 0 0 0 0 0；铡头地狱 50 0 0 0 0 0 0；逆之心情 40 0 0 0 0 0 0；钉板地狱 50 0 0 0 0 0 0；北海龙宫 70 0 0 0 0 0 0；剥皮地狱 50 0 0 0 0 0 0；犁春原 30 0 0 0 0 0 0；坠泪岗 10 0 0 0 0 0 0；羊肠道 30 0 0 0 0 0 0；空灵园 90 0 0 0 0 0 0；小房间 70 0 0 0 0 0 0；逆之情结 40 0 0 0 0 0 0；寒井台 30 0 0 0 0 0 0；净沙原 20 0 0 0 0 0 0；青风原 10 0 0 0 0 0 0；南海龙宫 70 0 0 0 0 0 0；吓死人洞窟1 10 0 0 0 0 0 0；西天界 90 0 0 0 0 0 0；东海?
-//盟军圈地掷骰子成功返回〓10012〓7〓2017-06-26 20:30:25〓254〓0〓36〓4936〓12〓0〓无〓战神台 80 76 3 23 3 25 2；铜人殿
-//盟军圈地掷骰子成功返回〓10123〓9〓2017-06-27 08:50:42〓325〓0〓0〓4365〓123〓7〓遇到强盗〓战神台 80 76 3 23 4 25 2；铜人殿 20 0 0 0 0 0
-                    //最新坐标
-                    最新坐标(split[1]);
-                    //点数
-                    int dianShu = str2Int(split[2]);
-                    //下次可操作时间
-                    mTimeCanOper = getDate(split[3]);
-                    //机动力
-                    mTimes = str2Int(split[4]);
-                    //未知
-                    int un = str2Int(split[5]);
-                    //花费
-                    int cost = str2Int(split[6]);
-                    //资金
-                    mZijin = str2Int(split[7]);
-                    //地皮编号
-                    int index = str2Int(split[8]);
-                    //特殊事件的数值
-                    int un2 = str2Int(split[9]);
-                    //特殊事件
-                    String event = split[10];
-                    log("最新坐标:" + mCurMapZuobiao + "-点数：" + dianShu
-                            + "-下次可操作时间：" + mTimeCanOper.toString()
-                            + "-当前时间：" + new Date().toString()
-                            + "-机动力：" + mTimes + "-未知字段：" + un
-                            + "-花费：" + cost + "-资金：" + mZijin
-                            + "-地皮编号：" + index + "-特殊事件的数值：" + un2
-                            + "-特殊事件：" + event
-                    );
-                    Map map = mMaps.get(index - 1);
-                    log(map.toString());
-                    processMapData(split[11]);
-                    if (!map.BuildIfGood()) {
-                        try {
-                            Thread.sleep(2500);
-                            setCmd(cmdZhiSZ);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                    onZhiShaiZiRsp(split);
+
                     break;
                 case "盟军圈地建造升级返回":
 //盟军圈地建造升级返回〓20009〓920〓弄梅洞窟 80 4 1 0 0 0 0〓80
@@ -345,24 +290,112 @@ public class QuanDi implements Runnable {
                     if (myHouses.size() < 7 && mCurMapZuobiao < 20000) {
                         Map map2 = mMaps.get(mCurMapIndex - 1);
                         if (!map2.BuildIfGood()) {
+                            zhiShaiZi();
+                        }
+                    } else {
+                        zhiShaiZi();
+                    }
+                    break;
+                default:
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
                             try {
-                                Thread.sleep(2500);
-                                setCmd(cmdZhiSZ);
+                                Thread.sleep(1000 * 60 * 3);
+                                setCmd(cmdReadP);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
                         }
-                    } else {
-                        try {
-                            Thread.sleep(2500);
-                            setCmd(cmdZhiSZ);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
+                    }).start();
             }
 
+        }
+    }
+
+    private void onZhiShaiZiRsp(String[] split) {
+//盟军圈地掷骰子成功返回〓10012〓7〓2017-06-26 20:30:25〓254〓0〓36〓4936〓12〓0〓无〓战神台 80 76 3 23 3 25 2；铜人殿
+//盟军圈地掷骰子成功返回〓10123〓9〓2017-06-27 08:50:42〓325〓0〓0〓4365〓123〓7〓遇到强盗〓战神台 80 76 3 23 4 25 2；铜人殿 20 0 0 0 0 0
+        Map map;//最新坐标
+        最新坐标(split[1]);
+        //点数
+        int dianShu = str2Int(split[2]);
+        //下次可操作时间
+        mTimeCanOper = getDate(split[3]);
+        //机动力
+        mTimes = str2Int(split[4]);
+        //未知
+        int un = str2Int(split[5]);
+        //花费
+        int cost = str2Int(split[6]);
+        //资金
+        mZijin = str2Int(split[7]);
+        //地皮编号
+        int index = str2Int(split[8]);
+        //特殊事件的数值
+        int un2 = str2Int(split[9]);
+        //特殊事件
+        String event = split[10];
+        log("最新坐标:" + mCurMapZuobiao + "-点数：" + dianShu
+                + "-下次可操作时间：" + mTimeCanOper.toString()
+                + "-当前时间：" + new Date().toString()
+                + "-机动力：" + mTimes + "-未知字段：" + un
+                + "-花费：" + cost + "-资金：" + mZijin
+                + "-地皮编号：" + index + "-特殊事件的数值：" + un2
+                + "-特殊事件：" + event
+        );
+        processMapData(split[11]);
+        map = mMaps.get(index - 1);
+        log(map.toString());
+        if (!map.BuildIfGood()) {
+            zhiShaiZi();
+        }
+    }
+
+    private void onReadPersonInfoRsp(String[] split) {
+        //帮派读取圈地副本返回〓hy814〓0〓4〓1000〓〓2017年5月22日9时25分44秒〓120〓7
+//帮派读取圈地副本返回〓hy814〓0〓47〓0〓〓2017年6月26日19时22分9秒〓240〓7
+//帮派读取圈地副本返回〓hy814〓20005〓47〓4930〓2017-06-26 19:28:09〓2017年6月26日19时33分27秒〓243〓
+//帮派读取圈地副本返回〓hy814〓10091〓4〓7320〓2017-06-26 19:02:12〓2017年6月26日20时51分43秒〓26〓7
+//帮派读取圈地副本返回〓hy812〓10025〓78〓2721〓2017-06-27 11:45:52〓2017年6月27日10时20分15秒〓137〓7
+        //账号
+        mAccount = split[1];
+        //最新坐标
+        最新坐标(split[2]);
+        //用户id
+        mId = str2Int(split[3]);
+        //圈地资金
+        mZijin = str2Int(split[4]);
+        //下次可操作时间
+        mTimeCanOper = getDate(split[5]);
+        //当前时间
+        mTimeCur = getDate2(split[6]);
+        //机动力
+        mTimes = str2Int(split[7]);
+        //本周个人最高可占领地皮数
+        mMaxBuild = str2Int(split[8]);
+        log("最新坐标:" + mCurMapZuobiao
+                + "-用户id：" + mId
+                + "-下次可操作时间：" + mTimeCanOper.toString()
+                + "-当前时间：" + mTimeCur.toString() + getWeekDays()
+                + "-机动力：" + mTimes
+                + "-资金：" + mZijin
+                + "-本周个人最高可占领地皮数：" + mMaxBuild
+        );
+        if (mZijin <= 0) {
+            setCmd(cmdEnable);
+        } else {
+            sendCmd(cmdReadS);
+        }
+    }
+
+    private void zhiShaiZi() {
+        try {
+            Thread.sleep(2500);
+            postMsg("圈地-掷 普通 骰子");
+            setCmd(cmdZhiSZ);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
@@ -438,6 +471,7 @@ public class QuanDi implements Runnable {
         if (mCurMapIndex > 0) {
             Map map1 = mMaps.get(mCurMapIndex - 1);
             log(map1.toString());
+            postMsg("圈地-位置："+map1.showInfo());
         } else {
             log("你还没出门呢");
         }
@@ -464,6 +498,15 @@ public class QuanDi implements Runnable {
             mFlagConnect = connect;
             log("检测连接状态:" + mFlagConnect);
         }
+    }
+
+    public void postMsg(String msg) {
+        postMsg(LogStr.newInstance(msg));
+    }
+
+    public void postMsg(LogStr event) {
+//        EventUtil.post(event);
+        log("app日志显示："+event.logMsg);
     }
 
     public boolean isConnected() {
@@ -567,13 +610,48 @@ public class QuanDi implements Runnable {
                     "地图='" + name + '\'' +
                             ", 序号='" + zuobiao + '\'' +
                             ", 价格='" + price + '\'' +
-                            ", 用户1='" + user1 + '\'' +
+                            ", 用户1='" + getUser(user1) + '\'' +
                             ", 用户1等级=" + levle1 +
                             ", 用户2='" + user2 + '\'' +
                             ", 用户2等级=" + levle2 +
                             ", 用户3='" + user3 + '\'' +
                             ", 用户3等级=" + levle3 + "\r\n"
                     ;
+        }
+
+        public String showInfo() {
+            return
+                    "地图=" + name + "\t 序号=" + zuobiao + "\t价格=" + price + "\t用户1=" + getUser(user1)
+                            + "   " + getLevel(levle1) +
+                            "\t用户2='" + getUser(user2)  + "   "  + getLevel(levle2) +
+                            "\t用户3='" + getUser(user3)  + "   "  + getLevel(levle3)
+                    ;
+        }
+
+        private String getUser(int user) {
+            if (user==mId) {
+                return "我";
+            }
+            return String.valueOf(user);
+        }
+
+        private String getLevel(int lelvel) {
+            switch (lelvel) {
+                case 0:
+                default:
+                    return "";
+                case 1:
+                    return "☆";
+                case 2:
+                    return "☆☆";
+                case 3:
+                    return "☆☆☆";
+                case 4:
+                    return "☆☆☆☆";
+                case 5:
+                    return "☆☆☆☆☆";
+
+            }
         }
 
         public boolean BuildIfGood() {
